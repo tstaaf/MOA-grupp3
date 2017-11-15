f1ea67385b14d134c8690ac81bca8dd7091d1c6f
tstaaf <timmyjs@hotmail.com>
2017-11-15 09:42:47 +0100

    sal och kursklasser
    fint

---
+++ NyttMOA/NyttMOA/Classroom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyttMOA
+{
+    class Classroom
+    {
+        public string Name { get; set; }
+        public int Seats { get; set; }
+        public string Course { get; set; }
+
+        public Classroom(string name, int seats, string course)
+        {
+            Name = name;
+            Seats = seats;
+            Course = course;
+        }
+        
+    }
+}
