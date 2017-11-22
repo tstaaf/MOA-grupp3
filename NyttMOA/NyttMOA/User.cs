1f18ae34676de998306e2444bb13fa1ce99d1be5
JonteNeander <jonte.neander@gmail.com>
2017-11-22 14:01:21 +0100

    user
    user

--- NyttMOA/NyttMOA/User.cs
+++ NyttMOA/NyttMOA/User.cs
@@ -20,24 +20,103 @@ namespace NyttMOA
             Password = password;
         }
 
+        public abstract void showMenu();
     }
 
     public class Admin : User
     {
         public Admin(string name, string username, string password) : base(name, username, password)
         {
-            
+
         }
 
+        public override void showMenu()
+        {
+            var menuChoice = true;
+
+            while (menuChoice)
+            {
+                Console.WriteLine("[1] Add Student");
+                Console.WriteLine("[2] Add Teacher");
+                Console.WriteLine("[3] Add Course");
+                Console.WriteLine("[4] Add classroom / View booked classrooms");
+                Console.WriteLine("[5] View Grades");
+                Console.WriteLine("[6] Exit");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+
+                    case "4":
+
+                        break;
+
+                    case "5":
+
+                        break;
+
+                    case "6":
+                        menuChoice = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid selection, Try again");
+                        break;
+                }
+            }
+        }
     }
 
     public class Student : User
     {
-        public string Grade { get; set; }
-
-        public Student(string name, string username, string password, string grade) : base(name, username, password)
+        public Student(string name, string username, string password) : base(name, username, password)
         {
-            Grade = grade;
+
+        }
+
+        public override void showMenu()
+        {
+            var menuChoice = true;
+
+            while (menuChoice)
+            {
+                Console.WriteLine("[1] Schedule");
+                Console.WriteLine("[2] Grades");
+                Console.WriteLine("[3] Exit");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+                        menuChoice = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid selection, Try again");
+                        break;
+                }
+            }
         }
     }
 
@@ -46,6 +125,44 @@ namespace NyttMOA
         public Teacher(string name, string username, string password) : base(name, username, password)
         {
 
+        }
+
+        public override void showMenu()
+        {
+            var menuChoice = true;
+
+            while (menuChoice)
+            {
+                Console.WriteLine("[1] Add / Remove students to course");
+                Console.WriteLine("[2] Grades");
+                Console.WriteLine("[3] Classrooms / Courses");
+                Console.WriteLine("[4] Exit");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+
+                    case "4":
+                        menuChoice = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid selection, Try again");
+                        break;
+                }
+            }
         }
     }
 }
