915e193d46e444e454eb40a9f00c876b3327fcb2
EvilBengt <anton.jh@hotmail.com>
2017-11-28 15:09:26 +0100

    hej

--- NyttMOA/NyttMOA/Schedule.cs
+++ NyttMOA/NyttMOA/Schedule.cs
@@ -157,8 +157,7 @@ namespace NyttMOA
             {
                 output += i + Environment.NewLine;
             }
-            output += "----------";
-            return output;
+            return output + ";----------";
         }
     }
 
